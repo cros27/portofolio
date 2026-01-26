6de7f5d14f2b2a2378392daa05563c83bb2beeaf
cros27 <vdz7rzfkhf@privaterelay.appleid.com>
2026-01-26 19:31:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const toggle = document.getElementById("themeToggle");
 
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  toggle.textContent = document.body.classList.contains("dark")
-    ? "Light"
-    : "Dark";
+// LOAD THEME
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "light") {
+  document.body.classList.add("light");
+  toggleTheme.textContent = "DARK";
+}
+
+// TOGGLE THEME
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+    toggleTheme.textContent = "DARK";
+  } else {
+    localStorage.setItem("theme", "dark");
+    toggleTheme.textContent = "LIGHT";
+  }
 });
+
